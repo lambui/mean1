c9856a2d34e8c9ed994f4b6130946bb0c474ec06
Lam <spot.lam@gmail.com>
2017-07-23 22:45:13 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//include frameworks
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const cors = require("cors"); //allow request made from different domain name
+const passport = require("passport");
+const mongoose = require("mongoose");
+
+//include local files
+const users = require('./routes/users');
+const config = require('./config/database');
+
+//connect mongodb with mongoose
+mongoose.connect(config.database, {useMongoClient: true});
+mongoose.connection.on('connected', ()=>{
+    console.log('Connected to database ' + config.database);
+});
+mongoose.connection.on('error', (err)=>{
+    console.log('Database error:' + err);
+});
+
+//init express
+const app = express();
+
+//port number
+const port = 3000;
+
+//cors middleware
+app.use(cors()); //currently allow request made from any domains
+
+//set static folder (angular stuffs)
+app.use(express.static(path.join(__dirname, "public")));
+
+//body parser middleware
+app.use(bodyparser.json());
+
+//define routes
+app.use("/users", users);
+
+app.get("/", (req, res) => {
+    res.send("Invalid Endpoint!");
+});
+
+//start app on port
+app.listen(port, () => {
+    console.log("Server starts on port " + port);
+});
---
+++ routes/users.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+//routes
+router.get('/register', (req, res, next) => {
+    res.send("REGISTER");
+});
+
+router.post('/auth', (req, res, next) => {
+    res.send("AUTH");
+});
+
+router.get('/profile', (req, res, next) => {
+    res.send("PROFILE");
+});
+
+module.exports = router;
