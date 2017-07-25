5f9efc7933dd03587e6527c6d2cf678b4dc96248
Lam <spot.lam@gmail.com>
2017-07-24 19:58:29 -0700

    complete user register POST request

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 //routes
-router.get('/register', (req, res, next) => {
-    res.send("REGISTER");
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err)
+        {
+            res.json({
+                success: false,
+                msg: "Fail to register user! " + err
+            })
+        }
+        else
+        {
+            res.json({
+                success: true,
+                msg: "User registered!"
+            })
+        }        
+    });
 });
 
 router.post('/auth', (req, res, next) => {
